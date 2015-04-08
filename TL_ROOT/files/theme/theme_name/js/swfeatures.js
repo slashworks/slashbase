c48be97f6a047768c55fa499167ce7cfce12b0b1
Stefan Beutler <sbeutler@borowiakziehe.de>
2015-04-08 10:28:24 +0200

    reworked swfeatures implementation

---
+++ TL_ROOT/files/theme/theme_name/js/swfeatures.js
@@ -0,0 +1,14 @@
+/**
+ * domready event
+ */
+document.addEventListener("DOMContentLoaded", function() {
+    // Code
+}, false);
+
+
+/**
+ * load event
+ */
+document.addEventListener("load", function() {
+    // Code
+}, false);
